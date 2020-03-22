082f4362d248b40ecf589ddaf06a395ed422b23f
TOSS <tiago.medice@gft.com>
2020-03-22 21:10:13 +0100

    ref

--- src/main/java/com/medici/app/config/AutoConfiguration.java
+++ src/main/java/com/medici/app/config/AutoConfiguration.java
@@ -1,11 +1,16 @@
 package com.medici.app.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.gcp.data.firestore.repository.config.EnableReactiveFirestoreRepositories;
+import org.springframework.cloud.gcp.data.firestore.transaction.ReactiveFirestoreTransactionManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.reactive.TransactionalOperator;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import com.medici.app.entity.Book;
 import com.medici.app.entity.Expense;
@@ -19,5 +24,22 @@ import com.medici.app.repository.ExpensesRepository;
 @EnableTransactionManagement
 @EnableReactiveFirestoreRepositories(basePackageClasses = { ExpensesRepository.class, BookRepository.class })
 public class AutoConfiguration {
+
+	@Autowired
+	ReactiveFirestoreTransactionManager txManager;
+
+	@Bean
+	DefaultTransactionDefinition transactionDefinition() {
+
+		DefaultTransactionDefinition transactionDefinition = new DefaultTransactionDefinition();
+		transactionDefinition.setReadOnly(false);
+		return transactionDefinition;
+	}
+
+	@Bean
+	TransactionalOperator operator() {
+		TransactionalOperator operator = TransactionalOperator.create(this.txManager, transactionDefinition());
+		return operator;
+	}
 
 }
